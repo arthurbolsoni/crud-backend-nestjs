4d962261392957cc455c90ec833004610d605020
Arthur Bolsoni <arthurbolsoni@hotmail.com>
2022-10-06 05:04:26 -0300

    sample test

--- src/person/person.service.spec.ts
+++ src/person/person.service.spec.ts
@@ -1,11 +1,19 @@
+import { ModuleRef } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { BaseEntity, Repository } from 'typeorm';
+import { AddressModule } from '../address/address.module';
 import { PersonService } from './person.service';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('PersonService', () => {
   let service: PersonService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [AddressModule],
       providers: [PersonService],
     }).compile();
 
@@ -16,3 +24,5 @@ describe('PersonService', () => {
     expect(service).toBeDefined();
   });
 });
+
+
